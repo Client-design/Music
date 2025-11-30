f877d7d6ca9573f0423400d711a22a49b77ff54b
Client-design <rajtiwary098765@gmail.com>
2025-11-30 17:39:43 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 class MyDocument extends Document {
@@ -9,15 +9,6 @@ class MyDocument extends Document {
         <Head>
           {/* This is where you would link to external resources like Google Fonts
             or inject critical meta tags for SEO that must be server-rendered.
-
-            We are NOT including the Font Awesome CDN here because we switched
-            to React Icons, which is the modern approach.
-            
-            Example of linking a font (replace with your desired font):
-            <link 
-              href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" 
-              rel="stylesheet" 
-            /> 
           */}
           <meta name="description" content="Hozaak Music & Arts Festival - The biggest music festival in Barak Valley." />
         </Head>
